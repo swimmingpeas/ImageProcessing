fd00beabeb339276f11d01d3510390a5bf8ddbe2
swimmingpeas <79441288+swimmingpeas@users.noreply.github.com>
2021-06-05 08:53:18 -0500

    Add files via upload

---
+++ Summer C/ImageProcessing/ImageProcessing/FormStart.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ImageProcessing
+{
+    partial class FormStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonLoadImage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonLoadImage
+            // 
+            this.ButtonLoadImage.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ButtonLoadImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonLoadImage.Location = new System.Drawing.Point(249, 170);
+            this.ButtonLoadImage.Name = "ButtonLoadImage";
+            this.ButtonLoadImage.Size = new System.Drawing.Size(239, 78);
+            this.ButtonLoadImage.TabIndex = 0;
+            this.ButtonLoadImage.Text = "Load Image";
+            this.ButtonLoadImage.UseVisualStyleBackColor = false;
+            this.ButtonLoadImage.Click += new System.EventHandler(this.ButtonLoadImage_Click);
+            // 
+            // FormStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonLoadImage);
+            this.Name = "FormStart";
+            this.Text = "Start Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonLoadImage;
+    }
+}
+
---
+++ Summer C/ImageProcessing/ImageProcessing/FormStart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageProcessing
+{
+    public partial class FormStart : Form
+    {
+        public FormStart()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ButtonLoadImage_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
+    }
+}
