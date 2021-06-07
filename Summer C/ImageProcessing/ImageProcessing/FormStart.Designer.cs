edf20de3bf544ee4029e434c2fe4b3ca12683157
ly <ly@DESKTOP-GRLSMPK>
2021-06-07 03:37:36 -0500

    Load images

--- Summer C/ImageProcessing/ImageProcessing/FormStart.Designer.cs
+++ Summer C/ImageProcessing/ImageProcessing/FormStart.Designer.cs
@@ -28,36 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ButtonLoadImage = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBoxOriginal = new System.Windows.Forms.PictureBox();
+            this.pictureBoxAfterEditing = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAfterEditing)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // ButtonLoadImage
+            // tableLayoutPanel1
             // 
-            this.ButtonLoadImage.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.ButtonLoadImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonLoadImage.Location = new System.Drawing.Point(249, 170);
-            this.ButtonLoadImage.Name = "ButtonLoadImage";
-            this.ButtonLoadImage.Size = new System.Drawing.Size(239, 78);
-            this.ButtonLoadImage.TabIndex = 0;
-            this.ButtonLoadImage.Text = "Load Image";
-            this.ButtonLoadImage.UseVisualStyleBackColor = false;
-            this.ButtonLoadImage.Click += new System.EventHandler(this.ButtonLoadImage_Click);
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 160F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1091, 721);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.pictureBoxOriginal, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.pictureBoxAfterEditing, 0, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(925, 715);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // pictureBoxOriginal
+            // 
+            this.pictureBoxOriginal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxOriginal.Location = new System.Drawing.Point(3, 3);
+            this.pictureBoxOriginal.Name = "pictureBoxOriginal";
+            this.pictureBoxOriginal.Size = new System.Drawing.Size(919, 341);
+            this.pictureBoxOriginal.TabIndex = 0;
+            this.pictureBoxOriginal.TabStop = false;
+            this.pictureBoxOriginal.SizeChanged += new System.EventHandler(this.pictureBoxOriginal_SizeChanged);
+            this.pictureBoxOriginal.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxOriginal_MouseDown);
+            this.pictureBoxOriginal.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBoxOriginal_MouseUp);
+            // 
+            // pictureBoxAfterEditing
+            // 
+            this.pictureBoxAfterEditing.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxAfterEditing.Location = new System.Drawing.Point(3, 370);
+            this.pictureBoxAfterEditing.Name = "pictureBoxAfterEditing";
+            this.pictureBoxAfterEditing.Size = new System.Drawing.Size(919, 342);
+            this.pictureBoxAfterEditing.TabIndex = 1;
+            this.pictureBoxAfterEditing.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(934, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(154, 715);
+            this.panel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.button1, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.groupBox2, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 3;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 54F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(154, 715);
+            this.tableLayoutPanel3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(39, 676);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonLeft);
+            this.groupBox1.Controls.Add(this.buttonRight);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(148, 324);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select image file";
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Location = new System.Drawing.Point(35, 37);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(75, 23);
+            this.buttonLeft.TabIndex = 0;
+            this.buttonLeft.Text = "<";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Location = new System.Drawing.Point(35, 99);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(75, 23);
+            this.buttonRight.TabIndex = 1;
+            this.buttonRight.Text = ">";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(3, 333);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(148, 324);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Settings";
             // 
             // FormStart
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.ButtonLoadImage);
+            this.ClientSize = new System.Drawing.Size(1091, 721);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "FormStart";
             this.Text = "Start Form";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAfterEditing)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button ButtonLoadImage;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBoxOriginal;
+        private System.Windows.Forms.PictureBox pictureBoxAfterEditing;
     }
 }
 
--- Summer C/ImageProcessing/ImageProcessing/FormStart.cs
+++ Summer C/ImageProcessing/ImageProcessing/FormStart.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,15 +13,113 @@ namespace ImageProcessing
 {
     public partial class FormStart : Form
     {
+        int m_iCurrentSelectedFile = 0;
+        Bitmap m_CurrentEditingPictureBitmap = null;
+        int g_iPictureWidth = 520;
+        int g_iPictureHeight = 400;
+
+        int m_xPosBeforeDragging = 0;
+        int m_yPosBeforeDragging = 0;
+
+        int iOriginalPictureStart_X = 0;
+        int iOriginalPictureStart_Y = 0;
+
         public FormStart()
         {
             InitializeComponent();
+
+            g_iPictureWidth = pictureBoxOriginal.Width;
+            g_iPictureHeight = pictureBoxOriginal.Height;
+
+            pictureBoxOriginal.Image = new Bitmap(g_iPictureWidth, g_iPictureHeight);
+            loadSelectedFile();
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            m_iCurrentSelectedFile--;
+            loadSelectedFile();
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            m_iCurrentSelectedFile++;
+            loadSelectedFile();
+        }
+
+        private void loadSelectedFile()
+        {
+            string[] arrFiles = Directory.GetFiles("testpictures");
+            if (arrFiles.Length > 0)
+            {
+                if (m_iCurrentSelectedFile < 0)
+                    m_iCurrentSelectedFile += arrFiles.Length;
+                int iSelectIndex = m_iCurrentSelectedFile % arrFiles.Length;
+                m_CurrentEditingPictureBitmap = new Bitmap(arrFiles[iSelectIndex]);
+
+                if (m_CurrentEditingPictureBitmap.Height < g_iPictureHeight || m_CurrentEditingPictureBitmap.Width < g_iPictureWidth)
+                {
+                    MessageBox.Show("The picture has at least " + g_iPictureWidth + "*" + g_iPictureHeight + " pixels. Please select another picture.");
+                    return;
+                }
+
+                showOriginalPicture();
+            }
+            else
+            {
+                m_CurrentEditingPictureBitmap = null;
+                MessageBox.Show("Please check if there are files in testpictures folder");
+            }
+        }
+
+        private void showOriginalPicture()
+        {
+            for (int x = 0; x < g_iPictureWidth; x++)
+            {
+                for (int y = 0; y < g_iPictureHeight; y++)
+                {
+                    Color pixelColor = m_CurrentEditingPictureBitmap.GetPixel(iOriginalPictureStart_X + x, iOriginalPictureStart_Y + y);
+                    ((Bitmap)pictureBoxOriginal.Image).SetPixel(x, y, pixelColor);
+                }
+            }
+            pictureBoxOriginal.Refresh();
         }
 
 
-        private void ButtonLoadImage_Click(object sender, EventArgs e)
+        private void pictureBoxOriginal_MouseDown(object sender, MouseEventArgs e)
         {
-            MessageBox.Show("Hello World");
+            m_xPosBeforeDragging = e.X;
+            m_yPosBeforeDragging = e.Y;
+        }
+
+        private void pictureBoxOriginal_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (m_CurrentEditingPictureBitmap == null)
+                return;
+            iOriginalPictureStart_X += (m_xPosBeforeDragging - e.X);
+            iOriginalPictureStart_Y += (m_yPosBeforeDragging - e.Y);
+
+            if (iOriginalPictureStart_X < 0)
+                iOriginalPictureStart_X = 0;
+            if (iOriginalPictureStart_X >= m_CurrentEditingPictureBitmap.Width - g_iPictureWidth)
+                iOriginalPictureStart_X = m_CurrentEditingPictureBitmap.Width - g_iPictureWidth - 1;
+
+            if (iOriginalPictureStart_Y < 0)
+                iOriginalPictureStart_Y = 0;
+            if (iOriginalPictureStart_Y >= m_CurrentEditingPictureBitmap.Height - g_iPictureHeight)
+                iOriginalPictureStart_Y = m_CurrentEditingPictureBitmap.Height - g_iPictureHeight - 1;
+
+            showOriginalPicture();
+        }
+
+        private void pictureBoxOriginal_SizeChanged(object sender, EventArgs e)
+        {
+            g_iPictureWidth = pictureBoxOriginal.Width;
+            g_iPictureHeight = pictureBoxOriginal.Height;
+
+            pictureBoxOriginal.Image = new Bitmap(g_iPictureWidth, g_iPictureHeight);
+            loadSelectedFile();
         }
     }
+        
 }
